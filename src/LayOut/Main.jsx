a755abcd28c98ba9437040dc9a71f3ada00d2ac3
AbdullahWB <abdullah917828@gmail.com>
2023-05-02 10:53:37 +0600

    navigation done

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navigation from '../Pages/Shared/Navigation/Navigation';
+import Header from '../Pages/Shared/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            hello
+            <Navigation></Navigation>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
