74fae45ee5cc6b1d1867eafd2b9105478287c69f
AbdullahWB <abdullah917828@gmail.com>
2023-05-02 16:48:38 +0600

    added invitation

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
+import Invitation from '../Invitation/Invitation';
 
 const Home = () => {
     const [masters, setMasters] = useState([])
@@ -12,13 +13,19 @@ const Home = () => {
     console.log(masters);
     return (
         <div className='my-[130px]'>
-            <div className='w-[80%] mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-7'>
-                {
-                    masters.map(master => <Cart
-                        key={master.id}
-                        master={master}
-                    ></Cart>)
-                }
+            <div>
+                <h1 className='text-6xl text-gray-800 text-center font-light mb-10'>Our ProudFull <span className='text-primary font-bold '>Chefs</span></h1>
+                <div className='w-[80%] mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-7'>
+                    {
+                        masters.map(master => <Cart
+                            key={master.id}
+                            master={master}
+                        ></Cart>)
+                    }
+                </div>
+            </div>
+            <div className='w-[80%] mx-auto'>
+                <Invitation></Invitation>
             </div>
         </div>
     );
