dd0252d1acbc90b28ea870751f94ae2ab847e851
AbdullahWB <abdullah917828@gmail.com>
2023-05-04 18:34:08 +0600

    add env firebase key

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDucbQOVrTpbdtzwPlK_IR8mfdMqHMGVA",
-  authDomain: "food-lover-project.firebaseapp.com",
-  projectId: "food-lover-project",
-  storageBucket: "food-lover-project.appspot.com",
-  messagingSenderId: "758666202696",
-  appId: "1:758666202696:web:d1222fd9bb8bc4a420527d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
